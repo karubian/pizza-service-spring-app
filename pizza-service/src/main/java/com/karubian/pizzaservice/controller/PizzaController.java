cb306353f1b177901be300b2b5ab2241ee58f83e
Burak <burake1009@gmail.com>
2021-05-14 18:39:07 +0300

    Create the skeleton of pizza controller. Closes #3

---
+++ pizza-service/src/main/java/com/karubian/pizzaservice/controller/PizzaController.java
@@ -0,0 +1,33 @@
+package com.karubian.pizzaservice.controller;
+
+import com.karubian.pizzaservice.model.PizzaDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RequestMapping("/api/v1/pizza")
+@RestController
+public class PizzaController {
+    @GetMapping("/{pizzaId}")
+    public ResponseEntity<PizzaDto> getPizzaById(@PathVariable("pizzaId") UUID pizzaId){
+        //todo impl
+
+        return new ResponseEntity<>(PizzaDto.builder().build(), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity saveNewPizza(@RequestBody PizzaDto pizzaDto) {
+
+        //todo impl
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{pizzaId}")
+    public ResponseEntity updatePizzaById(@PathVariable("pizzaId") UUID pizzaId,@RequestBody PizzaDto pizzaDto)
+    {
+        //todo impl
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+}
